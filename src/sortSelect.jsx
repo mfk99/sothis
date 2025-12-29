9c7317e0a22bdca35b9a78f608382ab2658d87fe
Matti Kähkönen <matti.kahkonen99@gmail.com>
2025-12-29 14:03:04 +0200

    improve header elements

--- src/sortSelect.jsx
+++ src/sortSelect.jsx
@@ -1,15 +1,35 @@
 function SortSelect({ onChange }) {
   return (
-    <select
-      class="font-mono border-2 border-solid border-gray-300 p-4 rounded-2xl"
-      defaultValue="alphabetical"
-      onChange={onChange}
-    >
-      <option value="alphabetical">Alphabetical</option>
-      <option value="alphabetical-asc">Alphabetical, ascending</option>
-      <option value="playtime">Playtime</option>
-      <option value="playtime-asc">Playtime, ascending</option>
-    </select>
+    <div>
+      <div class="relative">
+        <select
+          class=" w-full h-10 pl-8 pr-3 text-sm text-gray-700  focus:outline-none  shadow-sm focus:border-blue-500  border-2 border-solid border-gray-300 rounded-2xl"
+          defaultValue="alphabetical"
+          onChange={onChange}
+        >
+          <option value="alphabetical">A-Z</option>
+          <option value="alphabetical-asc">Z-A</option>
+          <option value="playtime">Most played</option>
+          <option value="playtime-asc">Least played</option>
+        </select>
+        <span class="absolute inset-y-0 left-0 flex items-center justify-center ml-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="black"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+            />
+          </svg>
+        </span>
+      </div>
+    </div>
   );
 }
 
--- src/usercard.jsx
+++ src/usercard.jsx
@@ -1,12 +1,29 @@
 export function UserCard({ user }) {
-  // console.log(user)
-  const userName = "Username: " + user.personaname;
+  const userName = user.personaname;
 
   return (
-    <div class="font-mono border-2 border-solid border-gray-300 p-4 rounded-2xl">
-      <h2>
-        <strong> {userName} </strong>
-      </h2>
+    <div>
+      <div class="relative">
+        <h2 class="flex items-center w-full h-10 pl-8 pr-3 text-sm text-gray-700  focus:outline-none  shadow-sm focus:border-blue-500  border-2 border-solid border-gray-300 rounded-2xl">
+          <strong> {userName} </strong>
+        </h2>
+        <span class="absolute inset-y-0 left-0 flex items-center justify-center ml-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+        </span>
+      </div>
     </div>
   );
 }
